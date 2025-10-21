c191189c37252a6519bd3e1e9d7c8fd79efac864
PGross <paul.riedlingen@gmail.com>
2025-10-21 16:41:11 +0200

    added RPT to the benchmark

--- experiments/duckdb_with_bf.py
+++ experiments/duckdb_with_bf.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 
-
 root_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.insert(0, root_directory)
 
@@ -10,7 +9,8 @@ from config.benchmark.tpch import get_tpch_benchmark
 from config.benchmark.imdb import get_imdb_benchmark
 from config.benchmark.tpcds import get_tpcds_benchmark
 
-from config.systems.duckdb import DUCK_DB_BF_BASELINE, DUCK_DB_BF_V1, DUCK_DB_BF_X86, DUCK_DB_EARLY_PROBING
+from config.systems.duckdb import DUCK_DB_BF_BASELINE, DUCK_DB_BF_V1, DUCK_DB_BF_X86, DUCK_DB_EARLY_PROBING, \
+    DUCK_DB_BF_RPT
 from src.models import RunConfig
 from src.runner.experiment_runner import run
 
@@ -29,7 +29,7 @@ def main():
             {'n_threads': 8},
             # {'n_threads': 10},
         ],
-        'systems': [DUCK_DB_BF_V1,  DUCK_DB_BF_X86, DUCK_DB_BF_BASELINE],
+        'systems': [DUCK_DB_BF_V1,  DUCK_DB_BF_X86, DUCK_DB_BF_BASELINE, DUCK_DB_BF_RPT],
         'benchmarks': [get_imdb_benchmark()] ,
     }
     run(config)
